1faba15a1e5944bced139ba68624565d570e0e15
LooBaka <zhuyifan0717@gmail.com>
2026-02-23 14:31:46 -0500

    Updated GuiMain

--- src/main/java/edu/bsu/cs/GuiMain.java
+++ src/main/java/edu/bsu/cs/GuiMain.java
@@ -3,18 +3,26 @@ package edu.bsu.cs;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GuiMain extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hello GUI");
-        VBox root = new VBox(label);
-        Scene scene = new Scene(root, 300, 200);
+        Label label = new Label("Wikipedia Revision Viewer");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        stage.setTitle("Wikipedia Revision Viewer");
         stage.setScene(scene);
-        stage.setTitle("Wikipedia GUI");
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
